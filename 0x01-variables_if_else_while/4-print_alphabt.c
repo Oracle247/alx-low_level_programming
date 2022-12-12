ef8f6fe608e281c096f24b9e80b74aee497e342c
Codes Oracle <94334497+Oracle247@users.noreply.github.com>
2022-12-11 20:08:01 -0400

    task - 04

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,29 @@
+
+#include <stdio.h>
+#include <ctype.h>
+
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Description: 'a program  that prints the alphabet in lowercase'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char x;
+
+	for (x = 'A'; x <= 'Z'; x++)
+	{
+		char small = tolower(x);
+
+        if (small == 'q' || small == 'e')
+            continue;
+		putchar(small);
+	}
+	putchar('\n');
+	return (0);
+}
