f5373b21f612da8623c6515d079da7590c0465ca
Codes Oracle <94334497+Oracle247@users.noreply.github.com>
2022-12-11 20:32:18 -0400

    task - 06

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,26 @@
+
+#include <stdio.h>
+#include <ctype.h>
+
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Description: 'a program  that prints the alphabet in lowercase'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i + '0');
+  	}
+
+  	putchar('\n');
+	return (0);
+}
