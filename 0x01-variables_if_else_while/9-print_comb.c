f67a3e578c9ab5b8082b8ba83239e4b004298a58
Codes Oracle <94334497+Oracle247@users.noreply.github.com>
2022-12-11 20:47:15 -0400

    task - 09

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+
+#include <stdio.h>
+#include <ctype.h>
+
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Description: 'a program  that prints the alphabet in lowercase'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i + '0');
+		putchar(',');
+		putchar(' ');
+	}
+
+	putchar('\n');
+	return (0);
+}
